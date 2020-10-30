9257e5356a06e0040d2aeb5f0fda4ae3e0b757e5
CwwwwS <45662005+CwwwwS@users.noreply.github.com>
2020-10-30 23:51:39 +0800

    Update sub.py

--- sub.py
+++ sub.py
@@ -28,6 +28,7 @@ def login(s: requests.Session, username, password):
     }
     r = s.post("https://app.ucas.ac.cn/uc/wap/login/check", data=payload)
     print(password)
+    print(user)
     # print(r.text)
     if r.json().get('m') != "操作成功":
         print(r.text)
@@ -131,7 +132,7 @@ def report(username, password):
     
     print(datetime.now(tz=pytz.timezone("Asia/Shanghai")).strftime("%Y-%m-%d %H:%M:%S %Z"))
 
-    for i in range(randint(10, 20), 0, -1):
+    for i in range(randint(1, 3), 0, -1):
         print("\r等待{}秒后填报".format(i), end='')
         sleep(1)
 
